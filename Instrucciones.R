7972390b2ac29ee3cc27e6880833b69aa4d17abc
Alfonso Jungmann <ajungmann@banchilevida.cl>
2019-03-18 16:53:59 -0300

    MODIFICACIONES

--- Instrucciones.R
+++ Instrucciones.R
@@ -8,4 +8,6 @@ data1$id<-seq(1,nrow(data1))
 data1$model<-paste(paste(19,data1$model_year),strrep(0,8-str_length(data1$model_year)))
 
 #####blancos y espacios######
-data1$model_ajunstado<-gsub(" ","",data1$model)
+data1$model_ajustado<-gsub(" ","",data1$model)
+data1$indicador<- gregexpr(7,data1$model_ajustado)
+data1$indicador[data1$indicador=='0']<-NA
